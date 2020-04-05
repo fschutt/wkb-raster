045339835112f553337c5f5babb2ea2a4a52c4b5
Felix Schütt <felix.schuett@maps4print.com>
2020-04-05 14:35:08 +0200

    Add example for parsing raster data

--- examples/read.rs
+++ examples/read.rs
@@ -2,15 +2,17 @@ use wkb_raster::big_endian as be;
 
 fn main() {
 
-    println!("written scaleX:\t{:?}", be::parse_f64_be(b"4F1BE8B4814E4B3F"));
-    println!("written scaleY:\t{:?}", be::parse_f64_be(b"4F1BE8B4814E4BBF"));
-    println!("written ipX:\t{:?}", be::parse_f64_be(b"602CF9C592FF1F40"));
-    println!("written ipY:\t{:?}", be::parse_f64_be(b"74DA40A70D004940"));
+    println!("N48E008:\n");
+
+    println!("written scaleX:\t{:?}", be::parse_f64_be(b"37AFEAA3AF484B3F"));
+    println!("written scaleY:\t{:?}", be::parse_f64_be(b"37AFEAA3AF484B3F"));
+    println!("written ipX:\t{:?}", be::parse_f64_be(b"0000000000002040"));
+    println!("written ipY:\t{:?}", be::parse_f64_be(b"0000000000804840"));
     
     println!("\nshould be:\n");
     
-    println!("scaleX:\t{:?}", be::parse_f64_be(b"030F984F285A4B3F"));
-    println!("scaleY:\t{:?}", be::parse_f64_be(b"EF0E984F285A4B3F"));
-    println!("ipX:\t{:?}", be::parse_f64_be(b"875E8A23B8FE1F40"));
-    println!("ipY:\t{:?}", be::parse_f64_be(b"2FB48EFB28004940"));
+    println!("scaleX:\t{:?}", be::parse_f64_be(b"4F1BE8B4814E4B3F"));
+    println!("scaleY:\t{:?}", be::parse_f64_be(b"4F1BE8B4814E4BBF"));
+    println!("ipX:\t{:?}", be::parse_f64_be(b"602CF9C592FF1F40"));
+    println!("ipY:\t{:?}", be::parse_f64_be(b"74DA40A70D804840"));
 }
